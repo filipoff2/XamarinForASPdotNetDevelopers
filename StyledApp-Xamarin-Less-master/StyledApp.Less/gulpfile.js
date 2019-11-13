65c88a92b5285c51ada7874035296694507b2122
filipoff2 <filipoff@o2.pl>
2019-11-13 12:14:26 +0100

    https://medium.com/@benetskyybogdan/less-with-xamarin-forms-efb9ad76cc1a

---
+++ StyledApp-Xamarin-Less-master/StyledApp.Less/gulpfile.js
@@ -0,0 +1,14 @@
+﻿var gulp = require("gulp"),
+    fs = require("fs"),
+    less = require("gulp-less");
+
+
+gulp.task("Build Less File", function () {
+    return gulp.src('MainPage.less')
+        .pipe(less())
+        .pipe(gulp.dest('../StyledApp/Styles'));
+});
+
+
+
+gulp.task('default', ['Build Less File']); 
