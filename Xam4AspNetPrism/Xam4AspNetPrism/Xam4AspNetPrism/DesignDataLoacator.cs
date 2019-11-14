9ef26a34182b1de601811823f2b39f67bca04076
filipoff2 <filipoff@o2.pl>
2019-11-14 15:33:45 +0100

    ViewModel SWAP XAML

---
+++ Xam4AspNetPrism/Xam4AspNetPrism/Xam4AspNetPrism/DesignDataLoacator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xam4AspNetPrism.ViewModels;
+
+namespace Xam4AspNetPrism
+{
+    public static class DesignDataLoacator
+    {
+        public static MainPageViewModel MainPageViewModeBasic
+        {
+            get { return new MainPageViewModel(null) { Title = "Basic" }; }
+        }
+        public static MainPageViewModel MainPageViewModeBogush
+        {
+            get { return new MainPageViewModel(null) { Title = "Bogush Bogush Bogush" }; }
+        }
+    }
+}
