5ed46dabc7d4896c863ab3d9a362271801d2d910
filipoff2 <filipoff@o2.pl>
2019-11-15 14:30:04 +0100

    Ugly formatting

--- Xam4AspNetPrism/Xam4AspNetPrism/Xam4AspNetPrism/DesignDataLoacator.cs
+++ Xam4AspNetPrism/Xam4AspNetPrism/Xam4AspNetPrism/DesignDataLoacator.cs
@@ -15,5 +15,32 @@ namespace Xam4AspNetPrism
         {
             get { return new MainPageViewModel(null) { Title = "Bogush Bogush Bogush" }; }
         }
+
+        ///  UGLY  DUMMY DATA = no culture 
+        const double demoSum = 99.99;
+
+        public static MainPageViewModel MainPageViewModeDE
+        {
+            get { return new MainPageViewModel(null) { Title = "Deutsch", ButtonText = "Rechnungen".ToUpper() + "€" };  }
+        }
+
+        public static MainPageViewModel MainPageViewModePL
+        {
+            get { return new MainPageViewModel(null) { UglyCurtureTextForSample= "PL", Title = "Polski", ButtonText = "Faktury " + demoSum + "PLN" }; }
+        }
+
+        public static MainPageViewModel MainPageViewModeEL
+        {
+            get { return new MainPageViewModel(null) { Title = "ελληνικά", ButtonText = "τιμολόγια " }; } //usp... "forgot to add"
+        }
+
+        public static MainPageViewModel MainPageViewModeEn
+        {
+            get { return new MainPageViewModel(null) { UglyCurtureTextForSample = "UK", Title = "English", ButtonText = "Invoices " + "€ " + demoSum }; } //I'm ugly and I know 
+        }
+        public static MainPageViewModel MainPageViewModeEn2
+        {
+            get { return new MainPageViewModel(null) { UglyCurtureTextForSample = "UK", Title = "English2", ButtonText = "Invoices" + "£ " + demoSum }; }
+        }
     }
 }
--- Xam4AspNetPrism/Xam4AspNetPrism/Xam4AspNetPrism/ViewModels/MainPageViewModel.cs
+++ Xam4AspNetPrism/Xam4AspNetPrism/Xam4AspNetPrism/ViewModels/MainPageViewModel.cs
@@ -10,12 +10,50 @@ namespace Xam4AspNetPrism.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public string SubTitle => "sksksk";
+        public string SubTitle => "Let's do some ugly formatting:";
+
+        public string ButtonText { get; set; }
+
+        #region This You WONN'T get in Previewer
+        public string EntryText { get; set; }
+        public string UglyCurtureTextForSample { get; set; }
+
+        public string LabelText { get; set; }
+        #endregion
+
+        public DelegateCommand SubmitCommand { get; private set; }
 
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             Title = "Main Page";
+            SubmitCommand = new DelegateCommand(Submit, CanSubmit);
         }
+
+        #region This You WONN'T get in Previewer
+        void Submit()
+        {
+            //implement logic
+            // USGLY SAMPLE LOGIC = No cultureInfo
+            LabelText = Title + " " + EntryText + " £";
+
+            if (UglyCurtureTextForSample == "UK")
+            {
+                LabelText = Title + " £" + EntryText ;
+            }
+
+            if (UglyCurtureTextForSample == "PL")
+            {
+                LabelText = Title + EntryText + "PLN";
+            }
+
+            RaisePropertyChanged("LabelText");
+        }
+
+        bool CanSubmit()
+        {
+            return true;
+        }
+        #endregion
     }
 }
