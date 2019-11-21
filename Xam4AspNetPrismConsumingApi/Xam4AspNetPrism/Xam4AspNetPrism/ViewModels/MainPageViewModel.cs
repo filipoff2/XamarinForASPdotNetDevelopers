fd4a0805222ac5da4c2e5b4ec59264f5223a1085
filipoff2 <filipoff@o2.pl>
2019-11-21 16:13:03 +0100

    Consuming Api Init commit

---
+++ Xam4AspNetPrismConsumingApi/Xam4AspNetPrism/Xam4AspNetPrism/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using static Xam4AspNetPrism.DesignDataLoacator;
+
+namespace Xam4AspNetPrism.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public string SubTitle => "Let's do some ugly formatting:";
+
+        public string ButtonText { get; set; }
+
+        #region This You WONN'T get in Previewer
+        public string EntryText { get; set; }
+        public string UglyCurtureTextForSample { get; set; }
+
+        public string LabelText { get; set; }
+        #endregion
+
+        public ObservableCollection<Monkey> Monkeys { get; set; }
+
+        public DelegateCommand SubmitCommand { get; private set; }
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+            SubmitCommand = new DelegateCommand(Submit, CanSubmit);
+        }
+
+        #region This You WONN'T get in Previewer
+        void Submit()
+        {
+            //implement logic
+            // USGLY SAMPLE LOGIC = No cultureInfo
+            LabelText = Title + " " + EntryText + " € ";
+
+            if (UglyCurtureTextForSample == "UK")
+            {
+                LabelText = Title + " £" + EntryText ;
+            }
+
+            if (UglyCurtureTextForSample == "PL")
+            {
+                LabelText = Title + EntryText + "PLN";
+            }
+
+            RaisePropertyChanged("LabelText");
+        }
+
+        bool CanSubmit()
+        {
+            return true;
+        }
+        #endregion
+    }
+}
